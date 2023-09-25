757c1a3c7396e17cb29edccae45b6658668671ac
Vladislav <jkronz@rambler.ru>
2023-09-25 10:44:48 +0500

    Small update

--- app/src/main/java/com/example/simplenotesapp/domain/util/NotesOrder.kt
+++ app/src/main/java/com/example/simplenotesapp/domain/util/NotesOrder.kt
@@ -8,12 +8,4 @@ sealed class NotesOrder(val name: Int) {
 
     object Timestamp : NotesOrder(name = R.string.timestamp)
 
-//    companion object{
-//        val notesOrderMap = mapOf(
-//            Title to Title.name,
-//            Timestamp to Timestamp.name
-//        )
-//        val currentNotesOrder = mutableStateOf<NotesOrder>(Title)
-//    }
-
 }
