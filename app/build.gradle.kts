d0746073a9cfddf9d38e781cea0f0172fd5593ae
Vlad <jkronz@rambler.ru>
2023-12-11 22:26:06 +0500

    Version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 5
-        versionName = "1.1.2"
+        versionName = "1.2.0"
         resourceConfigurations += listOf("en")
 
         vectorDrawables {
